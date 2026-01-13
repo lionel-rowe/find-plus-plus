8a7730bfcd0f80fef8c844b65ad7b8ed399a760b
lionel-rowe <lionel.rowe@gmail.com>
2026-01-13 22:27:16 +0000

    Fix conflicts with Polymer library

--- src/defineCustomElement.ts
+++ src/defineCustomElement.ts
@@ -1,9 +1,21 @@
 import { CUSTOM_ELEMENT_NAME, TEMPLATE_ID } from './config.ts'
 import { assert } from '@std/assert/assert'
 
+/**
+ * This nonsense is necessary to avoid conflicts with polyfills bundled with
+ * [Polymer](https://github.com/Polymer/polymer),
+ * e.g. on youtube.com.
+ */
+const HTMLElement = globalThis.HTMLElement.prototype.constructor as typeof globalThis.HTMLElement
+const customElements = {
+	define: (globalThis.customElements.constructor.prototype.define as typeof globalThis.customElements.define).bind(
+		globalThis.customElements,
+	),
+}
+
 customElements.define(
 	CUSTOM_ELEMENT_NAME,
-	class extends HTMLElement {
+	class FindPlusPlus extends HTMLElement {
 		constructor() {
 			super()
 			const template = document.getElementById(TEMPLATE_ID)
